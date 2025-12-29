d98476e4374b420f3b8925d66cc83ceb6b96ead3
ryhndastra <ryhndastra@gmail.com>
2025-12-29 18:23:37 +0700

    apply welcome screen

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import FloatingGhosts from "@/components/effects/FloatingGhosts";
+import WelcomeScreen from "@/components/common/WelcomeScreen";
 
 export const metadata = {
   title: "GarudaPS – Growtopia Private Server",
@@ -21,6 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="bg-[#1a1a1a] text-slate-100">
+        <WelcomeScreen />
         <FloatingGhosts />
         <Navbar />
         {children}
