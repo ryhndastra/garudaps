6d72e65893f5d23d9ed2a2f251ceeccd0e60dc53
ryhndastra <ryhndastra@gmail.com>
2026-04-12 00:35:23 +0700

    remove floating character

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,17 +1,23 @@
-"use client"; 
+"use client";
 
 import dynamic from "next/dynamic";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import WelcomeScreen from "@/components/common/WelcomeScreen";
 
-const FloatingGhosts = dynamic(() => import("@/components/effects/FloatingGhosts"), { 
-  ssr: false 
-});
+// const FloatingGhosts = dynamic(
+//   () => import("@/components/effects/FloatingGhosts"),
+//   {
+//     ssr: false,
+//   },
+// );
 
-const FloatingButtons = dynamic(() => import("@/components/effects/FloatingButtons"), { 
-  ssr: false 
-});
+const FloatingButtons = dynamic(
+  () => import("@/components/effects/FloatingButtons"),
+  {
+    ssr: false,
+  },
+);
 
 export default function MainLayout({
   children,
@@ -21,14 +27,12 @@ export default function MainLayout({
   return (
     <>
       <WelcomeScreen />
-      
-      <FloatingGhosts />
-      
+
+      {/* <FloatingGhosts /> */}
+
       <div className="relative z-10 flex flex-col min-h-screen">
         <Navbar />
-        <main className="grow">
-          {children}
-        </main>
+        <main className="grow">{children}</main>
         <Footer />
       </div>
 
