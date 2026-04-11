f6122f2113070c22f3b2e4c883ee6e7873ceaca6
ryhndastra <ryhndastra@gmail.com>
2026-04-11 17:05:31 +0700

    lala

--- src/app/api/leaderboard/route.ts
+++ src/app/api/leaderboard/route.ts
@@ -1,8 +1,15 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  if (!process.env.api_leaderboard_url) {
+    return NextResponse.json(
+      { error: "Server misconfiguration" },
+      { status: 500 },
+    );
+  }
+
   try {
-    const res = await fetch("https://garuda.gtpscache.site/api/leaderboard", {
+    const res = await fetch(process.env.api_leaderboard_url, {
       cache: "no-store",
     });
 
@@ -11,14 +18,12 @@ export async function GET() {
     }
 
     const data = await res.json();
-    
     return NextResponse.json(data);
-    
   } catch (error) {
     console.error("Leaderboard Proxy Error:", error);
     return NextResponse.json(
       { error: "Failed to fetch leaderboard" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
--- src/app/api/proxy-status/route.ts
+++ src/app/api/proxy-status/route.ts
@@ -1,17 +1,26 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET() {
+  if (!process.env.api_online_status_url) {
+    return NextResponse.json(
+      { status: "offline", players: 0 },
+      { status: 500 },
+    );
+  }
+
   try {
-    const response = await fetch("https://garuda.gtpscache.site/api/status", {
-      cache: 'no-store', 
+    const response = await fetch(process.env.api_online_status_url, {
+      cache: "no-store",
     });
-    
+
     if (!response.ok) throw new Error("Gagal ambil data");
-    
+
     const data = await response.json();
     return NextResponse.json(data);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (error) {
-    return NextResponse.json({ status: "offline", players: 0 }, { status: 500 });
+  } catch {
+    return NextResponse.json(
+      { status: "offline", players: 0 },
+      { status: 500 },
+    );
   }
 }
